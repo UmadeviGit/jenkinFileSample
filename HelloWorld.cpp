bff5529353fe08b8e45d13ae715f86cec2c4ed7d
UmadeviGit <94173120+UmadeviGit@users.noreply.github.com>
2022-03-16 15:18:13 +0530

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	 cout << "Hello, world!" << endl;
+	 cout << "Hello, world 2!" << endl;
+	 cout << "Hello, world 3!" << endl;
+	 cout << "Hello, world !4" << endl;
+	 cout << "Hello, world !5" << endl;
+	 cout << "Hello, world !6" << endl;
+	cout << "Hello, world !7" << endl;
+	cout << "Hello, world !8" << endl;
+	cout << "Hello, world cpack" << endl;
+	cout << "Hello, world 07-03-2022 9:16AM" << endl;
+	return 0;
+}
